6256fcfcfff3c9d3bead41345c3b82dfea8c16d9
ThistleProse <thistleprose@gmail.com>
2026-02-21 20:13:05 +1100

    Update family.lang.php

--- inc/languages/english/family.lang.php
+++ inc/languages/english/family.lang.php
@@ -1,1 +1,76 @@
+<?php
+// navigation
+$l['family'] = "Family Control Panel";
+$l['family_navigation'] = "Navigation";
+$l['family_start'] = "Home page";
+$l['family_faq'] = "Instructions";
+$l['family_filter_families'] = "Filter families";
+$l['family_filter_member'] = "Filter family members";
+$l['family_control'] = "Administration";
+$l['family_add_family'] = "Add family";
+$l['family_add_member'] = "Add family member";
+$l['family_edit'] = "View/Edit family";
+$l['family_claims'] = "Family reservations";
 
+// content
+$l['family_welcome'] = "Welcome to the Families section of the forum! Here you can not only create your own family, but also search for existing family members – perhaps one of them will match one of your character ideas?";
+$l['family_add_family_desc'] = "Congratulations, you're taking your first step in creating your family! Start with the basics: what's your family's name, what social class do they belong to, and what else is there to say?";
+$l['family_add_familymember_desc'] = "Think about how far back in the family tree you want to go. If you want to start with your character's parents, that's Generation 1, and you yourself are Generation 2. If you start with the grandparents, then they are Generation 1, your parents are Generation 2, and you are Generation 3! Consider in advance which family generation your own character belongs to—perhaps they have children, nieces and nephews, or even grandchildren? Is your own character perhaps even Generation 1? You can really let your imagination run wild here and create as many generations as are important for your character. For your character's position in the family, it's best to always start with Generation 1! If Generation 1 is a mother, then Generation 2 could be a daughter or son, Generation 3 a grandson – and so on. You can specify further information in the short description.";
+$l['family_family'] = "Family";
+$l['family_numbers'] = "Statistics";
+$l['family_members'] = "Members";
+$l['family_free'] = "Free";
+$l['family_taken'] = "Claimed";
+$l['family_edit_text'] = "Edit";
+$l['family_edit_text'] = "Delete";
+$l['family_set_free'] = "Release";
+$l['family_full_age'] = "years old";
+$l['family_families_found'] = "Families found";
+$l['family_member_found'] = "found playable family members";
+$l['family_members_free'] = "Free members";
+$l['family_claim'] = "reserve";
+$l['family_info_free'] = "&raquo; Reserve!";
+$l['family_info_take'] = "&raquo; It's me!";
+$l['family_info_unplayable'] = "Unplayable!";
+$l['family_info_claimed'] = "Belongs to:";
+
+// forms
+$l['family_lastname'] = "Last name";
+$l['family_description'] = "Description";
+$l['family_class'] = "Class";
+$l['family_class_under'] = "Lower class";
+$l['family_class_middle'] = "Middle class";
+$l['family_class_high'] = "Upper class";
+$l['family_fullname'] = "First and Last Name";
+$l['family_gender'] = "Gender";
+$l['family_gender_male'] = "Male";
+$l['family_gender_female'] = "Female";
+$l['family_gender_diff'] = "Non-binary";
+$l['family_gender_indiff'] = "Other";
+$l['family_generation'] = "Generation";
+$l['family_position'] = "Position in the family";
+$l['family_age'] = "Age";
+$l['family_picture'] = "Picture";
+$l['family_playable'] = "Playable";
+$l['family_playable_yes'] = "Yes";
+$l['family_playable_no'] = "No";
+$l['family_add_yourself'] = "[ <strong><em>Important</em></strong>: You add yourself first! ]";
+$l['family_age_start'] = "Between";
+$l['family_age_end'] = "and";
+$l['family_wanted_link'] = "Request link";
+$l['family_wanted'] = "Wanted";
+
+// misc
+$l['family_family_added'] = "Your family has been added!";
+$l['family_claimed'] = "This character has been reserved for you!";
+$l['family_thread_text'] = "This is a family request! Click on the picture of the family member you are interested in to get more information. Feel free to reply in the thread if you are interested in taking someone on.";
+$l['family_date'] = "Reserved on";
+
+// Alerts
+$l['family_alert_claimed'] = "Your family member {2} has been reserved!";
+$l['myalerts_setting_claimed'] = "Receive notifications for newly reserved family members?"; 
+
+// online.php
+$l['family_wio_cp'] = "<a href=\"family.php\">Family Control Panel</a>";
+
+?>
